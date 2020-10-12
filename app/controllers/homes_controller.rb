4c9e45f60142a29d3964ce612dffab1fbd5f2fd6
Tako <takoforme@gmail.com>
2020-10-13 06:16:34 +0900

    Controllerとindexページの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,23 @@
 class HomesController < ApplicationController
   def index
   end
-end
 
+  def result
+    @name = params[:name]
+    @action = params[:action]
+    @error = params[:error]
+    @step = params[:step]
+    @error_code = params[:error_code]
+    @expect = params[:expect]
+    @reference = params[:reference]
+    @try = params[:try]
+    @result = params[:result]
+    @user_pc = params[:user_pc]
+    @environment = params[:environment]
+    @editor = params[:editor]
+    @programming_lang = params[:programming_lang]
+    @github_link = params[:github_link]
+    @signature = params[:signature]
+  end
+
+end
