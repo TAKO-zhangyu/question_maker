fd440fa416406ef87d9b482ea1e4a2fe93419d01
Tako <takoforme@gmail.com>
2020-10-20 22:40:08 +0900

    モデルを使用した設計に変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,23 +1,30 @@
 class HomesController < ApplicationController
-  def index
+
+  def new
+    @home = Home.new(home)
   end
 
   def result
-    @name = params[:name]
-    @action = params[:action]
-    @error = params[:error]
-    @step = params[:step]
-    @error_code = params[:error_code]
-    @expect = params[:expect]
-    @reference = params[:reference]
-    @try = params[:try]
-    @result = params[:result]
-    @user_pc = params[:user_pc]
-    @environment = params[:environment]
-    @editor = params[:editor]
-    @programming_lang = params[:programming_lang]
-    @github_link = params[:github_link]
-    @signature = params[:signature]
-  end
+    @params = params.slice(
+      :name,
+      :action,
+      :error,
+      :step,
+      :error_code,
+      :expect,
+      :reference,
+      :try,
+      :result,
+      :user_pc,
+      :environment,
+      :editor,
+      :programming_lang,
+      :github_link,
+      :signature
+    )
 
+    # home = params[:home].permit(:name, :action, :error, :step, :error_code, :expect, :reference, :try, :result, :user_pc, :environment, :editor, :programming_lang, :github_link, :signature)
+    # @home = Home.new(home)
+  end
+  
 end
---
+++ app/models/home.rb
@@ -0,0 +1,8 @@
+class Home
+  include ActiveModel::Model
+  attr_accessor :name, :action, :error, :step, :error_code, :expect, :reference, :try, :result, :user_pc, :environment, :editor, :programming_lang, :github_link, :signature
+
+
+  end
+end
+
