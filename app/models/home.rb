cdb315098a9003c8834ffafdc3a1e26c672b4573
Tako <takoforme@gmail.com>
2020-10-20 22:55:11 +0900

    homeモデル内の不要なendを削除

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,8 +1,5 @@
 class Home
   include ActiveModel::Model
   attr_accessor :name, :action, :error, :step, :error_code, :expect, :reference, :try, :result, :user_pc, :environment, :editor, :programming_lang, :github_link, :signature
-
-
-  end
 end
 
